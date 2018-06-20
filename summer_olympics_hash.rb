591d42e1aaf89d20e398254e1ee34df8a583fdb2
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-20 17:56:34 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,9 +1,9 @@
 
 def create_olympics_hash
   olympics_hash = {
-    :Sydney => "2000", 
-    :Athens => "2004", 
-    :Beijing => "2008", 
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
     :London => "2012"
   }
   return olympics_hash
@@ -28,7 +28,7 @@ def iterate_through_hash
      :London => "2012"
    }
   olympics_hash[:Atlanta] = "1996"
-  
+
   olympics_hash.each do |place, year|
     puts "The #{place} summer olympics took place in #{year}"
   end
@@ -42,14 +42,14 @@ def iterate_through_keys
      :London => "2012"
    }
   olympics_hash[:Atlanta] = "1996"
-  
+
   upcased_cities = []
   counter = 0
     olympics_hash.each_key |place|
-    upcased_cities[counter] = place.upcase 
-    counter += 1 
+    upcased_cities[counter] = place.upcase
+    counter += 1
   
   puts upcased_cities
 end
-  
+
 end
